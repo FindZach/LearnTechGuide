441ec46061e3dc28ab16f35b6b42d2bc24f42f30
FindZach <zach@findzach.com>
2023-06-17 10:53:31 -0400

    INIT COMMIT - LearnTechGuide FrontEnd

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+
+    // ...other configuration options
+    content: [
+      './src/**/*.html',
+    ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  }
+
