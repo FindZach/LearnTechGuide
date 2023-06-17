77ffd49561a9f8ab249627450fd480f00d492fb9
FindZach <zach@findzach.com>
2023-06-17 11:02:34 -0400

    Added modal component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ModalComponent } from './shared/componenets/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
